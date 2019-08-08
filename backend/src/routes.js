f7459482355443bceb633b6f9f790fd05b6d46b4
Daniel Fedatto <daniel.fedatto@gmail.com>
2019-08-08 18:07:17 -0300

    Like and dislike manangement

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,14 @@
 // Arquivo controlador de rotas
 const express = require('express');
+const DevController = require('./controllers/DevController');
+const LikeController = require('./controllers/LikeController');
+const DisLikeController = require('./controllers/DisLikeController');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: `Hello ${req.query.name}` });
-});
-
-routes.post('/devs' , (req, res) => {
-    return res.json(req.body);
-});
+routes.get('/devs' , DevController.index)
+routes.post('/devs' , DevController.store);
+routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', DisLikeController.store);
 
 module.exports = routes;
