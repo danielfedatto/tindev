1f5d268c667aeb0dc72780d87e18bd546b85fd16
Daniel Fedatto <daniel.fedatto@gmail.com>
2019-08-08 18:11:39 -0300

    Add package cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const routes = require('./routes');
 
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://daniel:0091598886c@cluster0-77aqb.mongodb.net/om
     useNewUrlParser: true
 });
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
