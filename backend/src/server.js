7a62b64a79beb5c42e5ae4a3999cc67e312afa8e
Daniel Fedatto <daniel.fedatto@gmail.com>
2019-08-08 18:05:36 -0300

    Make database connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes');
 
 const server = express();
 
-server.use(express.json());
+mongoose.connect('mongodb+srv://daniel:0091598886c@cluster0-77aqb.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
 
+server.use(express.json());
 server.use(routes);
 
 server.listen(3333);
