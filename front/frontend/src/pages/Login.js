c8cda17d236a9e2258201aa4fcacad911282494e
Daniel Fedatto <daniel.fedatto@gmail.com>
2019-08-09 16:21:56 -0300

    Page login developpement

--- front/frontend/src/pages/Login.js
+++ front/frontend/src/pages/Login.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
+
+import api from '../services/api';
 
 import logo from '../assets/logo.svg';
 
+// This history inside the function is a handle by react dom to redirect the user to the specific
+// page that i have created
 
-export default function Login() {
+export default function Login({ history }) {
+    const [username, setUsername] = useState('');
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        // Aqui a gente ja está conectando com a API criada pelo backend
+        const response = await api.post('/devs', {
+            username,
+        });
+
+        const { _id } = response.data;
+
+        history.push(`/dev/${_id}`);
+    }
+
     return (
         <div className="login-container">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <img src={logo} alt="Tindev" />
-                
+                <input 
+                    placeholder="Digite seu usuário do Github"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
+                <button type="submit">Enviar</button>
             </form>
 
         </div>
